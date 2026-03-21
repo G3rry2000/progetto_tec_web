094e298317209e7292408e5ef0940d23f6b919a0
GabryCix <cicconegabriele1@icloud.com>
2026-03-21 17:53:33 +0100

    Fix grafica css

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,9 @@
     <title>ASD Morra De Sanctis 🐺</title>
     
     <link rel="stylesheet" href="css/style.css">
-    
+    <?php if (isset($css_extra)): ?>
+        <link rel="stylesheet" href="css/<?php echo $css_extra; ?>">
+    <?php endif; ?>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
